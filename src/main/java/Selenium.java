181c3c78ffa1613f8ac1ef2c1ebc3c54bbd672c4
nikola <nikola@yopmail.com>
2024-06-04 20:07:21 +0200

    Inicijalno pravljenje direktorijuma

---
+++ src/main/java/Selenium.java
@@ -0,0 +1,33 @@
+import dev.failsafe.internal.util.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver_125.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+
+        System.out.println(driver.getTitle());
+
+
+        driver.get("https://www.google.com");
+        driver.findElement(By.cssSelector("[name='q']")).sendKeys("Torte i kolači");
+        driver.findElement(By.cssSelector("[name='btnK']")).click();
+
+
+        Thread.sleep(5000);
+
+
+
+        driver.quit();
+    }
+}
